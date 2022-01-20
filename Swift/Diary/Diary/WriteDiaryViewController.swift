0a64865040a6cfd2101358ad2f1d97297a7676aa
BeomsangKim <choas0869@naver.com>
2022-01-20 16:16:25 +0900

    Add Diary App UI

--- Swift/Diary/Diary/WriteDiaryViewController.swift
+++ Swift/Diary/Diary/WriteDiaryViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class WriteDiaryViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureContentsTextView()
+    }
+    
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 255/255)
+        self.contentsTextView.layer.borderColor = borderColor.cgColor
+        self.contentsTextView.layer.borderWidth = 0.5
+        self.contentsTextView.layer.cornerRadius = 5.0
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: Any) {
     }
 }
