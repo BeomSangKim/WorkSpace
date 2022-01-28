a76f5d3e105e48a7f86f2a22177c60c2c01ee502
BeomsangKim <choas0869@naver.com>
2022-01-29 03:09:17 +0900

    COVID19-현황  - UI Add

--- Swift/COVID19-현황/COVID19-현황/CovidDetailViewController.swift
+++ Swift/COVID19-현황/COVID19-현황/CovidDetailViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class CovidDetailViewController: UITableViewController {
 
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UIView!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var occurePercentageCell: UITableViewCell!
+    @IBOutlet weak var overseaInflowCell: UITableViewCell!
+    @IBOutlet weak var regionOutbreakCell: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
