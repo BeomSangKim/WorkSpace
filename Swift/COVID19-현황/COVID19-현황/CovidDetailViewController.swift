7c2a42add2389706f0e54989780d16fe4b823ee8
BeomsangKim <choas0869@naver.com>
2022-01-29 02:56:34 +0900

    COCID19 - 현황 APP Start - cocoapods add Alarmofire, Charts

---
+++ Swift/COVID19-현황/COVID19-현황/CovidDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19-현황
+//
+//  Created by Bumsang Kim on 2022/01/28.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Swift/COVID19-현황/Pods/Target Support Files/Pods-COVID19-현황/Pods-COVID19-현황-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_COVID19_______ : NSObject
+@end
+@implementation PodsDummy_Pods_COVID19_______
+@end
